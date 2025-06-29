39023b61e15e1960c638ec27f096f059959376ed
JHANSIRANI41 <127426371+JHANSIRANI41@users.noreply.github.com>
2025-06-29 21:25:12 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from "react";
+https://code-help-backend-resq.onrender.comimport { useState, useRef, useEffect } from "react";
 import "prismjs/themes/prism-tomorrow.css";
 import Editor from "react-simple-code-editor";
 import prism from "prismjs";
